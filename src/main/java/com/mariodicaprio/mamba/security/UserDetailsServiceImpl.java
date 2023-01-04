999484b0b672468b2ce4743e8080c02aafe68941
MarioDiCaprio <mariodicaprio10@gmail.com>
2023-01-04 17:41:06 +0100

    Updated logging for UserDetailsService

--- src/main/java/com/mariodicaprio/mamba/security/UserDetailsServiceImpl.java
+++ src/main/java/com/mariodicaprio/mamba/security/UserDetailsServiceImpl.java
@@ -3,6 +3,7 @@ package com.mariodicaprio.mamba.security;
 
 import com.mariodicaprio.mamba.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 
 @Service
+@Log4j2
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 
@@ -19,7 +21,13 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        var user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username));
+        log.info("Fetching user details for username \"" + username + "\"");
+        var user = userRepository.findByUsername(username).orElse(null);
+        if (user == null) {
+            log.trace("User with username \"" + username + "\" not found");
+            throw new UsernameNotFoundException(username);
+        }
+        log.info("User with username \"" + username + "\" found successfully");
         return new UserDetailsImpl(user);
     }
 
