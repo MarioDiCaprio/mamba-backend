916410d985e3899bb16cf316652a7d3ba1f61be5
MarioDiCaprio <mariodicaprio10@gmail.com>
2023-01-23 01:58:06 +0100

    Updated RSA key system

--- src/main/java/com/mariodicaprio/mamba/MambaApplication.java
+++ src/main/java/com/mariodicaprio/mamba/MambaApplication.java
@@ -1,14 +1,11 @@
 package com.mariodicaprio.mamba;
 
 
-import com.mariodicaprio.mamba.security.RSAKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 
 @SpringBootApplication
-@EnableConfigurationProperties(RSAKeyProperties.class)
 public class MambaApplication {
 
     public static void main(String[] args) {
