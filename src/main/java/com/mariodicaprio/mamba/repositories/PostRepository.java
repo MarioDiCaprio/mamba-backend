c82f33140913b0b5521672bc5381b3d492b7ddc2
MarioDiCaprio <mariodicaprio10@gmail.com>
2023-02-11 11:36:29 +0100

    Updated post repository

--- src/main/java/com/mariodicaprio/mamba/repositories/PostRepository.java
+++ src/main/java/com/mariodicaprio/mamba/repositories/PostRepository.java
@@ -5,10 +5,13 @@ import com.mariodicaprio.mamba.entities.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, UUID> {
+
+    List<Post> findByTitle(String title);
 
 }
